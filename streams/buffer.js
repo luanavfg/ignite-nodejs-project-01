e8692e4c5bd410bf6b61746fca5b26a3983f0d1d
Luana <luanavefago1@gmail.com>
2023-08-31 08:16:46 -0300

    feat: buffer fundamentals

---
+++ streams/buffer.js
@@ -0,0 +1,15 @@
+// Buffer é a representação de um espaço na memória do computador,
+// usado especificamente para transitar dados de forma muito rápida.
+
+// Salvar, ler da memória e excluir, de maneira performática
+// -> É mais performático você ler parcialmente uma informação de forma binária, que é como o buffer guarda
+
+// JS nunca teve uma forma nativa de trabalhar com dados binários
+
+const buf = BUffer.from("ok");
+
+// hexadecimal
+console.log(buf);
+
+// decimal
+console.log(buf.toJSON());
