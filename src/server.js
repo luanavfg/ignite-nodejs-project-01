2b1eb3f205e096896be2cf5bc212519883b94b61
Luana <luanavefago1@gmail.com>
2023-09-06 08:31:28 -0300

    feat: add routes with regex params

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 // CommonJS => const http = require('http');
 // ESModule
+
+// Query Parameters: URL Stateful => Filtros, paginação, busca
+// http://localhost:3333/users?userId=1&name=Luana
+// Não deve ser usado para dados sensíveis
+
+// Route Parameters: Identificação de recurso
+// GET http://localhost:3333/users/1 fica subentendida a intenção de puxar usuário de id 1
+// Não deve ser usado para dados sensíveis
+
+// Request Body: envio de informação de um formulário
+// Usam protocolo HTTPs -> maior segurança
+
+// ---------------------------------------------------------
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
@@ -10,10 +23,12 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+    console.log(routeParams);
     return route.handler(req, res);
   }
   return res.writeHead(404).end();
